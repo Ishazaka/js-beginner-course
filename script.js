4aa524553bac3d875a380e99cf9024c31d0e49d4
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2021-11-19 02:50:04 +0500

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,15 @@ console.log(b);
 var a = '5673';
 var b = Number(a);
 console.log(b);
+
+
+// parseInt (it gives you value without decimals)
+
+var a = '99.56';
+var b = parseInt(a);
+console.log(b);
+
+var a = 'hello';
+var b = parseInt(a);
+console.log(b);
+
