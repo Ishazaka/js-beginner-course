aa9a0131a5279c6212ef599a1469eba889e78d48
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2021-11-19 01:23:43 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+
+// Number Methods
+
+//Number (converts the value into numnber)
+
+var a = '99';
+var b = Number(a);
+console.log(b);
+
+var a = 'hello';
+var b = Number(a);
+console.log(b);
+
+var a = '5673';
+var b = Number(a);
+console.log(b);
